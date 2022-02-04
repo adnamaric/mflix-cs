015b1235dca3826e947bbd04be915e8e61dc2826
Adna Maric <adna.maric@edu.fit.ba>
2022-02-04 13:16:43 +0100

    Lab 14: Timeouts

--- M220NTests/Constants.cs
+++ M220NTests/Constants.cs
@@ -4,7 +4,7 @@ namespace M220NTests
 {
     public static class Constants
     {
-        public static string MongoDbConnectionUriWithMaxPoolSize = MongoDbConnectionUri() + "&maxPoolSize=50";
+        public static string MongoDbConnectionUriWithMaxPoolSize = MongoDbConnectionUri() + "&maxPoolSize=50"  + "&WTimeoutMS=2500";
 
         public static string MongoDbConnectionUri()
         {
